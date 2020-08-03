6e2d03c737cb289ef83c2a23bc8381a57d01ae66
ulises <ulisescamps9@gmail.com>
2020-08-02 23:49:13 -0500

    getting all user data with their id

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Button, Spinner } from "react-bootstrap";
+import {withRouter} from "react-router-dom"
+import BasicLayout from "../../layouts/BasicLayout"
+import { getUserApi } from "../../api/user";
+import { toast } from "react-toastify";
+
 import "./User.scss"
 
-export default function User() {
+function User(props) {
+
+    const {match} = props
+    const [user, setuser] = useState(null)
+
+    useEffect(() => {
+        getUserApi(match.params.id).then(response => {
+            setuser(response)
+            if (!response) toast.error("El usuario que has visitado no existe")
+        }).catch(() => {
+            toast.error("El usuario no existe")
+        })
+    }, [match.params])
+
     return (
-        <div>
-            <h2>.l.</h2>
-        </div>
+        <BasicLayout className="user">
+            <div className="user__title">
+                <h2>User..</h2>
+            </div>
+            <div>Banner User</div>
+            <div>Info User</div>
+            <div className="user__tweets">List of tweets</div>
+        </BasicLayout>
     )
 }
+
+export default withRouter(User)
