13577822c8d21f1bc7b150138421d68328497032
ulises <ulisescamps9@gmail.com>
2020-08-07 03:01:50 -0500

    Fixing logout error

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,80 +1,88 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { Button, Spinner } from "react-bootstrap";
-import {withRouter} from "react-router-dom"
-import BasicLayout from "../../layouts/BasicLayout"
+import { withRouter } from "react-router-dom";
+import BasicLayout from "../../layouts/BasicLayout";
 import { getUserApi } from "../../api/user";
 import { getUserTweetsApi } from "../../api/tweet";
 import { toast } from "react-toastify";
-import BannerAvatar  from "../../components/User/BannerAvatar";
-import useAuth from "../../hooks/useAuth"
-import InfoUser  from "../../components/User/InfoUser";
+import BannerAvatar from "../../components/User/BannerAvatar";
+import useAuth from "../../hooks/useAuth";
+import InfoUser from "../../components/User/InfoUser";
 import ListTweets from "../../components/ListTweets";
 
-import "./User.scss"
+import "./User.scss";
 
 function User(props) {
+  const { match, setRefreshCheckLogin } = props;
+  const [user, setUser] = useState(null);
+  const [tweets, setTweets] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loadingTweets, setLoadingTweets] = useState(false);
+  const { params } = match;
+  const loggedUser = useAuth();
 
-    const {match} = props
-    const [user, setuser] = useState(null)
-    const [tweets, setTweets] = useState(null)
-    const [page, setPage] = useState(1)
-    const [loadingTweets, setLoadingTweets] = useState(false)
-    const {params} = match
-    const loggedUser = useAuth()
+  useEffect(() => {
+    getUserApi(params.id)
+      .then((response) => {
+        if (!response) toast.error("El usuario que has visitado no existe");
+        setUser(response);
+      })
+      .catch(() => {
+        toast.error("El usuario no existe");
+      });
+  }, [params]);
 
-    useEffect(() => {
-        getUserApi(params.id).then(response => {
-            if (!response) toast.error("El usuario que has visitado no existe")
-            setuser(response)
-        }).catch(() => {
-            toast.error("El usuario no existe")
-        })
-    }, [params])
+  useEffect(() => {
+    getUserTweetsApi(params.id, 1)
+      .then((response) => {
+        setTweets(response);
+      })
+      .catch(() => {
+        setTweets([]);
+      });
+  }, [params]);
 
-    useEffect(() => {
-        getUserTweetsApi(params.id, 1)
-        .then(response => {
-            setTweets(response)
-        }).catch(() => {
-            setTweets([])
-        })
-    }, [params])
-
-    const moreData = () => {
-        const pageTemp = page + 1
-        setLoadingTweets(true)
-        getUserTweetsApi(params.id, pageTemp).then(response => {
-            if(!response) {
-                setLoadingTweets(0)
-            }else {
-                setTweets([...tweets, ...response])
-                setPage(pageTemp)
-                setLoadingTweets(false)
-            }
-        })
-    }
-    return (
-        <BasicLayout className="user">
-            <div className="user__title">
-            <h2>
-                {user ? `${user.name} ${user.surname}` : "Este usuario no existe"}</h2>
-            </div>
-            <BannerAvatar user={user} loggedUser={loggedUser}/>
-            <InfoUser user ={user}/>
-            <div className="user__tweets">
-                <h3>Tweets</h3>
-                {tweets && <ListTweets tweets={tweets}/>}
-                <Button onClick={moreData}>
-                    {!loadingTweets ? (
-                        loadingTweets !== 0 && 'Obtener mas Tweets'
-                    ) : (
-                        <Spinner as="span" animation="grow" size="sm" 
-                        role="status" aria-hidden="true"/>
-                    )}
-                </Button>
-            </div>
-        </BasicLayout>
-    )
+  const moreData = () => {
+    const pageTemp = page + 1;
+    setLoadingTweets(true);
+    getUserTweetsApi(params.id, pageTemp).then((response) => {
+      if (!response) {
+        setLoadingTweets(0);
+      } else {
+        setTweets([...tweets, ...response]);
+        setPage(pageTemp);
+        setLoadingTweets(false);
+      }
+    });
+  };
+  return (
+    <BasicLayout className="user" setRefreshCheckLogin={setRefreshCheckLogin}>
+      <div className="user__title">
+        <h2>
+          {user ? `${user.name} ${user.surname}` : "Este usuario no existe"}
+        </h2>
+      </div>
+      <BannerAvatar user={user} loggedUser={loggedUser} />
+      <InfoUser user={user} />
+      <div className="user__tweets">
+        <h3>Tweets</h3>
+        {tweets && <ListTweets tweets={tweets} />}
+        <Button onClick={moreData}>
+          {!loadingTweets ? (
+            loadingTweets !== 0 && "Obtener mas Tweets"
+          ) : (
+            <Spinner
+              as="span"
+              animation="grow"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+          )}
+        </Button>
+      </div>
+    </BasicLayout>
+  );
 }
 
-export default withRouter(User)
+export default withRouter(User);
