9bd1d34e2ebf19e87a76809da3697243a6ff1bfd
ulises <ulisescamps9@gmail.com>
2020-08-07 03:48:02 -0500

    changing user type (follows, news) and user search

--- src/page/Users/Users.js
+++ src/page/Users/Users.js
@@ -3,16 +3,23 @@ import { Spinner, ButtonGroup, Button } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 import queryString from "query-string";
 import { isEmpty } from "lodash";
+import { useDebouncedCallback } from "use-debounce";
 import { getFollowsApi } from "../../api/follow";
 import BasicLayout from "../../layouts/BasicLayout";
 import ListUsers from "../../components/ListUsers";
 import "./Users.scss";
 
 function Users(props) {
-  const { setRefreshCheckLogin, location } = props;
+  const { setRefreshCheckLogin, location, history } = props;
   const [users, setUsers] = useState(null);
   const params = useUsersQuery(location);
-
+  const [typeUser, setTypeUser] = useState(params.type || "follow");
+  const [onSearch] = useDebouncedCallback((value) => {
+    setUsers(null);
+    history.push({
+      search: queryString.stringify({ ...params, search: value, page: 1 }),
+    });
+  }, 200);
   useEffect(() => {
     getFollowsApi(queryString.stringify(params))
       .then((response) => {
@@ -26,8 +33,19 @@ function Users(props) {
         setUsers([]);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [location]);
 
+  const onChangeType = (type) => {
+    setUsers(null);
+    if (type === "new") {
+      setTypeUser("new");
+    } else {
+      setTypeUser("follow");
+    }
+    history.push({
+      search: queryString.stringify({ type: type, page: 1, search: "" }),
+    });
+  };
   return (
     <BasicLayout
       className="users"
@@ -36,12 +54,26 @@ function Users(props) {
     >
       <div className="users__title">
         <h2>Usuarios</h2>
-        <input type="text" placeholder="Busca un usuario" />
+        <input
+          type="text"
+          placeholder="Busca un usuario"
+          onChange={(e) => onSearch(e.target.value)}
+        />
       </div>
 
       <ButtonGroup className="users__options">
-        <Button>Siguiendo</Button>
-        <Button>Nuevos</Button>
+        <Button
+          className={typeUser === "follow" && "active"}
+          onClick={() => onChangeType("follow")}
+        >
+          Siguiendo
+        </Button>
+        <Button
+          className={typeUser === "new" && "active"}
+          onClick={() => onChangeType("new")}
+        >
+          Nuevos
+        </Button>
       </ButtonGroup>
       {!users ? (
         <div className="users__loading">
