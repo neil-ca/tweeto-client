b62dbdd9c6666af8a34bb0a3657245327eee64d0
ulises <ulisescamps9@gmail.com>
2021-03-10 23:59:44 -0600

    getting tweets of followers

--- components/tweets.js
+++ components/tweets.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
-import { addTweet, getUserTweets } from "../services/tweet"
+import { addTweet, getTweetsFollowers, getUserTweets } from "../services/tweet"
 import styles from '../styles/home.module.scss'
 
 export default function Tweets({ token, id }) {
     const [message, setMessage] = useState('')
     const [tweets, setTweets] = useState([])
+    const [twFollowers, setTwFollowers] = useState([])
     const maxLength = 280
     const handle = (e) => {
         e.preventDefault()
@@ -20,22 +21,28 @@ export default function Tweets({ token, id }) {
         getUserTweets(id, 1, token).then(
             setTweets
         ).catch(err => { <h1>{err}</h1> })
+        getTweetsFollowers(1, token).then(
+            setTwFollowers
+        )
     }, [])
     return (
         <div className={styles.ctn_tweets}>
             <section className={styles.create_tweet}>
-            <textarea placeholder="What's happening?" onChange={e => setMessage(e.target.value)} rows="4" cols="30" required />
-            <button onClick={handle}>tweet</button>
+                <textarea placeholder="What's happening?" onChange={e => setMessage(e.target.value)} rows="4" cols="30" required />
+                <button onClick={handle}>tweet</button>
             </section>
             {tweets != null ?
                 <section className={styles.list_tweets}>
-                    {tweets.map((tweet) => ( 
-                       // return (
-                            <div key={tweet._id}>
-                                <h1>{tweet.message}</h1>
-                            </div>
+                    {tweets.map((tweet) => (
+                        // return (
+                        <div key={tweet._id}>
+                            <h1>{tweet.message}</h1>
+                        </div>
                         // )   
                     ))}
+                    {twFollowers != null ? twFollowers.map((twf) => (
+                        <h1>{twf}</h1>
+                    )) : <h1>No tweets of followers</h1>}
                 </section>
                 : <h1>No tweets</h1>}
         </div>
