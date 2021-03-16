284fbb77b031c3bf923bbf48771c51a75f5462fc
ulises <ulisescamps9@gmail.com>
2021-03-15 20:21:38 -0500

    fixing buntton for following users

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -9,12 +9,21 @@ import Cookie from 'js-cookie'
 
 export default function Profile() {
     const [profile, setProfile] = useState()
+    const [following, setFollowing] = useState(null)
+
     const router = useRouter()
     const id = router.query.id
     const token = Cookie.get('token')
     // const id = Cookie.get('id')
-    const handle = () => {
-        unfollowUser(id, token)
+    const ofFollow = () => {
+        unfollowUser(id, token).then(() => {
+            setFollowing(false)
+        })
+    }
+    const onFollow = () => {
+        followUser(id, token).then(() => {
+            setFollowing(true)
+        })
     }
     useEffect(() => {
         if (token) {
@@ -23,10 +32,17 @@ export default function Profile() {
             }).catch((err) => {
                 <h1>{err}</h1>
             })
+            checkFollow(id, token).then(res => {
+                if (res?.status) {
+                    setFollowing(true)
+                } else {
+                    setFollowing(false)
+                }
+            })
         } else {
             router.push('/login')
         }
-    }, [])
+    }, [following])
     return (<>
         <div className={styles.nav}>
             <Link href="/home"><a>Tweeto <AiOutlineTwitter /></a></Link>
@@ -39,7 +55,9 @@ export default function Profile() {
                         <h2>{profile.biography}</h2>
                         <h3>{profile.email} {profile.location}</h3>
                         <h4>{profile.dateofbirth}</h4>
-                        <button onClick={handle}>unfollow<AiOutlineUserAdd /></button>
+                        {following ? 
+                        <button onClick={ofFollow}>Following <AiOutlineUserAdd/></button>
+                        : <button onClick={onFollow}>Follow <AiOutlineUserAdd/></button>}
                     </div>
             }
         </div>
